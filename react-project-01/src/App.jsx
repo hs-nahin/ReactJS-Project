0bf9a06de09d718580e7e38edd352f26fcb7bddd
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-02-05 04:10:30 +0600

    task background, shadow, and hover added

--- react-project-01/src/App.jsx
+++ react-project-01/src/App.jsx
@@ -6,12 +6,12 @@ function App() {
 
   const handleClick1 = () => {
     setIsSelected1(!isSelected1);
-    setIsSelected2(false); // Ensure the other button is not selected
+    setIsSelected2(false);
   };
 
   const handleClick2 = () => {
     setIsSelected2(!isSelected2);
-    setIsSelected1(false); // Ensure the other button is not selected
+    setIsSelected1(false); 
   };
   
   return (
@@ -19,7 +19,7 @@ function App() {
       <div className="w-full md:w-3/5 bg-gray-800 rounded-lg shadow-md">
         {/* Header Name */}
         <header id='header-area' className="py-5">
-          <h1 className="text-3xl text-center font-semibold text-blue-300">
+          <h1 className="text-3xl text-center font-semibold text-blue-300 hover:text-pink-700 duration-300 ease-in-out">
             Na-Dos
           </h1>
         </header>
@@ -32,13 +32,13 @@ function App() {
             <div className="mb-3 md:mb-0">
               <label htmlFor="title" className='mr-5 text-blue-300'>Title</label>
               <br />
-              <input type="text" id="title" placeholder='Add Your Task Name' name="title" className='bg-gray-700 rounded-sm text-blue-300 outline-none w-full md:w-64 h-10 px-4'/>
+              <input type="text" id="title" placeholder='Add Your Task Name' name="title" className='bg-gray-700 rounded-sm text-blue-300 outline-none w-full md:w-64 h-10 px-4 hover:shadow-xl duration-300'/>
             </div>
             {/* Description */}
             <div className='ml-0 md:ml-5'>
               <label htmlFor="description" className='mr-5 text-blue-300'>Description</label>
               <br />
-              <input type="text" id="description" placeholder="Add Your Descriptions for the task" name="description" className="bg-gray-700 rounded-sm text-blue-300 outline-none w-full md:w-72 h-10 px-4" />
+              <input type="text" id="description" placeholder="Add Your Descriptions for the task" name="description" className="bg-gray-700 rounded-sm text-blue-300 outline-none w-full md:w-72 h-10 px-4 hover:shadow-xl duration-300" />
             </div>
             {/* Button */}
             <div>
@@ -55,9 +55,10 @@ function App() {
             <button className={`flex-1 bg-gray-600 text-blue-100 p-2 rounded-r-sm rounded-l-none transition duration-300 ${isSelected1 ? 'bg-pink-800' : ''}`} onClick={handleClick1}>Todo</button>
               <button className={`flex-1 bg-gray-700 text-blue-100 p-2 rounded-r-sm rounded-l-none transition duration-300 ${isSelected2 ? 'bg-sky-800' : ''}`} onClick={handleClick2}>Completed</button>
             </div>
-            <div id="todo-list" className='mb-10 mt-5'>
-              <h3 className='text-3xl mt-2 text-blue-300'>Task 1</h3>
-              <p className='text-2xl mt-1 text-blue-300'>Description</p>
+            {/* Task Area */}
+            <div id="todo-list" className='mb-10 mt-5 bg-slate-700 shadow-md hover:shadow-xl ease-in-out duration-300'>
+              <h3 className='text-3xl mt-2 ml-5 pt-5 text-blue-300'>Task 1</h3>
+              <p className='text-2xl mt-1 ml-5 pb-5 text-blue-300'>Description</p>
             </div>
           </footer>
         </main>
