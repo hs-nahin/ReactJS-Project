4ee4d870092c186965b58223fd9a390f9b09c1eb
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-03-08 21:57:59 +0600

    typewriter animation added

--- react-project-04/src/components/Home/Banner/Banner.jsx
+++ react-project-04/src/components/Home/Banner/Banner.jsx
@@ -1,12 +1,14 @@
 // Banner Component
 import 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 const Banner = () => {
     return (
-        <header className="flex flex-col-reverse sm:flex-row items-center justify-center sm:justify-between px-4 py-8 sm:px-0">
+        <header className="flex flex-col-reverse sm:flex-row items-center justify-center sm:justify-between px-4 sm:px-0 bg-purple-50">
             {/* First Part */}
             <div className="sm:w-1/2 sm:ml-10 lg:ml-36 text-center sm:text-left">
-                <h1 className="text-4xl sm:text-5xl lg:text-6xl font-semibold text-gray-700 mb-4 sm:mb-6 lg:mb-8 leading-loose" style={{ lineHeight: '5.1rem' }}>One Step<br />Closer To Your<br /> <span className="text-purple-500">Dream Job</span></h1>
+                <h1 className="text-4xl sm:text-5xl lg:text-6xl font-semibold text-gray-700 mb-4 sm:mb-6 lg:mb-8 leading-loose" style={{ lineHeight: '5.1rem' }}>One Step<br />Closer To Your<br />
+                <TypeAnimation className="text-purple-500" sequence={['Dream Job',1000,]}wrapper="span"speed={50}style={{ fontSize: '2em', display: 'inline-block' }}repeat={Infinity}/></h1>
                 <p className="text-lg sm:text-3xl mb-4 sm:mb-6 lg:mb-8 text-gray-500">Explore diverse career paths with detailed guidance to streamline your job search and seize opportunities seamlessly. Discover your future today!</p>
                 <div className="flex justify-center sm:justify-start mb-4 sm:mb-6 lg:mb-8">
                     <button className="py-2 sm:py-3 px-6 sm:px-10 rounded-sm bg-gradient-to-r from-purple-500 to-indigo-500 text-white hover:from-indigo-600 hover:to-purple-600 transition duration-500 ease-in-out transform hover:scale-105">Get Started</button>
--- react-project-04/src/components/Navigation/Navigation.jsx
+++ react-project-04/src/components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center text-gray-600'>
       <div className='flex gap-10'>
         <NavLink to="/" className={({ isActive }) => `transition ease-in-out duration-300 ${isActive ? 'text-transparent bg-gradient-to-r from-purple-600 to-indigo-800 bg-clip-text' : ''} hover:text-transparent hover:bg-gradient-to-r hover:from-purple-600 hover:to-indigo-800 hover:bg-clip-text`}>
           Home
