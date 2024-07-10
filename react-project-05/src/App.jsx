77fb49193a69f380dc707bfcee6dccc202ec3315
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-07-10 18:36:30 +0600

    basic context functionality : DONE

--- react-project-05/src/App.jsx
+++ react-project-05/src/App.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
 import "./App.css";
+import { TodoProvider } from "./TodoContext";
 
 function App() {
+  // State
+  const [todos, setTodos] = useState([]);
+  // Add Todo
+  const addTodo = (todo) => {
+    setTodos((prev) => [...prev, {id: Date.now(), ...todo}])
+  };
+  // Update Todo
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+  // Delete Todo
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+  // Toggle Todo
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  }
   return (
-    <>
+    <TodoProvider value={{addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
@@ -14,7 +34,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </TodoProvider>
   );
 }
 
