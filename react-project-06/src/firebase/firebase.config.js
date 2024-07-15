fd8367d2eb2ede698a0e730f8227ee366cfa1597
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-07-16 01:22:17 +0600

    dragon news project using firebase and react router

---
+++ react-project-06/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkeilczWe-6Q36U4KgeXBSXzKGppd2xss",
+  authDomain: "dragon-news-ee53f.firebaseapp.com",
+  projectId: "dragon-news-ee53f",
+  storageBucket: "dragon-news-ee53f.appspot.com",
+  messagingSenderId: "15834858364",
+  appId: "1:15834858364:web:673b34912079a14aa9daf9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
